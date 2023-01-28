b9880c7c152f7fd1431e0ccbaeca49f326214b96
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2023-01-28 20:50:00 +0100

    commit

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function darktheme() {
     root.style.setProperty('--neutral-bg-blue', "hsl(230, 17%, 14%)");
     root.style.setProperty('--neutral-top-bg-blue', "hsl(232, 19%, 15%)");
     root.style.setProperty('--neutral-card-bg-blue', "hsl(228, 28%, 20%)");
+    root.style.setProperty('--neutral-card-bg-hover', "hsl(228, 28%, 30%)");
     root.style.setProperty('--neutral-text-grey', "hsl(228, 34%, 66%)");
     root.style.setProperty('--neutral-white', "hsl(0, 0%, 100%)");
 }
@@ -30,6 +31,7 @@ function darktheme() {
     root.style.setProperty('--neutral-bg-blue', "hsl(0, 0%, 100%)");
     root.style.setProperty('--neutral-top-bg-blue', "hsl(225, 100%, 98%)");
     root.style.setProperty('--neutral-card-bg-blue', "hsl(227, 47%, 96%)");
+    root.style.setProperty('--neutral-card-bg-hover', "hsl(227, 47%, 86%)");
     root.style.setProperty('--neutral-text-grey', "hsl(228, 12%, 44%)");
     root.style.setProperty('--neutral-white', "hsl(230, 17%, 14%)");
 }
